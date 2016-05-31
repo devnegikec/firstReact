167293ceef70ed39017f6590df5d0c5498af1b33
Devendra Negi <devnegi_kec@yahoo.co.in>
2016-05-31 22:08:11 +0530

    React setup

---
+++ README.md
@@ -0,0 +1,6 @@
+>npm init
+>npm i react react-dom -save
+>npm i babel-loader babel-core babel-preset-es2015 babel-preset-react
+>create index.html App.js main.js webpack.config.js
+>npm i babel webpack webpack-dev-server --save
+
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+devtool: 'source-map',
+entry: './main.js',
+output:{
+    path:'./',
+    filename: 'index.js',
+},
+devServer: {
+    inline: true,
+    port: 3333
+},
+module: {
+loaders:[
+{
+    test: /\.js$/,
+    exclude: /node_modules/,
+    loader: 'babel',
+    query:{
+        presets: ['es2015','react']
+    }
+    }
+]
+},
+	
+}
